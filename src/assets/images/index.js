0c7666eb882325e6f8f2d59423efac3018e25b9c
jorgequintero <jquintedori@gmail.com>
2023-11-27 16:09:27 -0500

    Initial Commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,23 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import prestti from './prestti.jpg'
+import scotia from './scotia.png'
+import pinterest from './pinterest.png'
+import autopistas from './autopistas.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    prestti,
+    scotia,
+    pinterest,
+    autopistas
+}
---
+++ src/constants/index.js
@@ -0,0 +1,227 @@
+import { prestti, scotia, autopistas, pinterest } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Full Stack Developer",
+        company_name: "Autopistas de la Sabana",
+        icon: autopistas,
+        iconBg: "#b7e4c7",
+        date: "October 2013 - December 2020",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+        ],
+    },
+    {
+        title: "React.js Developer",
+        company_name: "Prestti Latam",
+        icon: prestti,
+        iconBg: "#accbe1",
+        date: "January 2021 - March 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Fullstack Software Engineer",
+        company_name: "Scotia Bank",
+        icon: scotia,
+        iconBg: "#fbc3bc",
+        date: "March 2022 - October 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Pinterest",
+        icon: pinterest,
+        iconBg: "#ffffff",
+        date: "December 2022 - August 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/jorgequinterodoria',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/jorgequinterodoria',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: motion,
+        theme: 'btn-back-red',
+        name: 'Full Stack Notion Clone',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/jorgequinterodoria/notion-clone',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Full Stack Spotify Clone',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/jorgequinterodoria/spotify-prueba',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Deliveroo React Native Clone',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/jorgequinterodoria/Deliveroo-ReactNative-Clone',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Gamification',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/jorgequinterodoria/hepatitis_game',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Signal React Native Clone',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/jorgequinterodoria/Signal',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Hulu Clone',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/jorgequinterodoria/hulu',
+    }
+];
